cc9eb4e9379c1c4919ed9e3a3eeeee9cff946f6f
Elmeri <Elmeri@ELMUW10P2>
2017-10-08 01:21:48 +0300

    added MaterialData.ReadFromFile

--- Orakel/Types/Material.cs
+++ Orakel/Types/Material.cs
@@ -7,6 +7,31 @@ using System.Globalization;
 
 namespace Orakel
 {
+    public enum Material
+    {
+        Air,
+        Acid,
+        Flesh,
+        Rock,
+        Concrete,
+        Wood,
+        WoodPlanks,
+        Plastic,
+        Cardboard,
+        Grass,
+        Ground,
+        Mud,
+        Water,
+        Snow,
+        Ice,
+        Glass,
+        GlassPane,
+        Metal,
+        MetalGrate,
+        Sand,
+        Fabric
+    }
+
     public struct MaterialData
     {
         private static Dictionary<Material, MaterialData> _materialAttributes;
@@ -34,7 +59,8 @@ namespace Orakel
                 f = float.Parse(fs, CultureInfo.InvariantCulture.NumberFormat);
                 e = float.Parse(es, CultureInfo.InvariantCulture.NumberFormat);
 
-                matAtt.Add(Material.Parse(mat), new MaterialData(d, f, e));
+                Material curMat = (Material)Enum.Parse(typeof(Material), mat, true);
+                matAtt.Add(curMat, new MaterialData(d, f, e));
             }
             return matAtt;
         }
@@ -54,64 +80,11 @@ namespace Orakel
             }
         }
 
-/*
-        public static Dictionary<Material, MaterialData> MaterialAttributes = new Dictionary<Material, MaterialData>()
-        {
-            // MATERIAL,                 Density, Friction, Elasticity
-            { Material.Air, new MaterialData(0.0f, 0.01f, 0.01f) },
-            { Material.Acid, new MaterialData(1.0f, 0.05f, 0.01f) },
-            { Material.Cardboard, new MaterialData(0.25f, 0.28f, 0.35f) },
-            { Material.Concrete, new MaterialData(2.403f, 0.7f, 0.2f) },
-            { Material.Fabric, new MaterialData(0.7f, 0.35f, 0.05f) },
-            { Material.Flesh, new MaterialData(0.975f, 0.35f, 0.05f) },
-            { Material.Glass, new MaterialData(1.619f, 0.28f, 0.15f) },
-            { Material.GlassPane, new MaterialData(1.619f, 0.25f, 0.25f) },
-            { Material.Grass, new MaterialData(0.9f, 0.4f, 0.1f) },
-            { Material.Ground, new MaterialData(0.9f, 0.4f, 0.1f) },
-            { Material.Ice, new MaterialData(0.819f, 0.02f, 0.15f) },
-            { Material.Metal, new MaterialData(7.85f, 0.4f, 0.25f) },
-            { Material.MetalGrate, new MaterialData(5.85f, 0.5f, 0.25f) },
-            { Material.Mud, new MaterialData(0.9f, 0.4f, 0.1f) },
-            { Material.Plastic, new MaterialData(0.7f, 0.3f, 0.5f) },
-            { Material.Rock, new MaterialData(2.691f, 0.4f, 0.2f) },
-            { Material.Sand, new MaterialData(1.602f, 0.5f, 0.05f) },
-            { Material.Snow, new MaterialData(0.819f, 0.25f, 0.15f) },
-            { Material.Water, new MaterialData(1.0f, 0.05f, 0.01f) },
-            { Material.Wood, new MaterialData(0.35f, 0.48f, 0.2f) },
-            { Material.WoodPlanks, new MaterialData(0.35f, 0.48f, 0.2f) }
-        };
-*/
-
         public MaterialData(float d, float f, float e)
         {
             Density = d;
             Friction = f;
             Elasticity = e;
         }
-    }
-
-    public enum Material
-    {
-        Air,
-        Acid,
-        Flesh,
-        Rock,
-        Concrete,
-        Wood,
-        WoodPlanks,
-        Plastic,
-        Cardboard,
-        Grass,
-        Ground,
-        Mud,
-        Water,
-        Snow,
-        Ice,
-        Glass,
-        GlassPane,
-        Metal,
-        MetalGrate,
-        Sand,
-        Fabric
     }
 }
